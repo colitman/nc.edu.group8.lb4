979e0cb28bef80dbe8f487f2aaceffb9082c92ce
hdls <myedumail@mail.ru>
2014-05-06 16:57:15 +0300

    #18 Убрал комменты вообще (лень переводить)

--- module-web/src/main/webapp/javascript.js
+++ module-web/src/main/webapp/javascript.js
@@ -1,22 +1,20 @@
-// Упрощаем получение документа
+
 function d(el) {
 	return document.getElementById(el);
 } 
 
-// Функция для закрытия модального окна. Code: href="javascript:close_modal()"
 function close_modal() {
-	d('modal_open').style.display = 'none'; // Скрыть модальное окно
-	d('modal_open').innerHTML = ''; // Удалить контент  в модальном окне
-	d('modal').style.display = 'none'; // Скрыть серый фон со страницы
+	d('modal_open').style.display = 'none'; 
+	d('modal_open').innerHTML = ''; 
+	d('modal').style.display = 'none'; 
 }
 
-// Функция для открытия модального окна. Code: href="javascript:open_modal(el,w,h)"
 function open_modal(el,w,h) {
-	d('modal').style.display = 'block'; // Нанести серый фон на страницу
-	d('modal_open').style.marginTop = '-'+h+'px'; // h - Переместить окно вверх, прописываеться в пикселях
-	d('modal_open').style.marginLeft = '-'+w+'px'; // w - Переместить окно влево, прописываеться в пикселях
-	d('modal_open').style.display = 'block'; // Показать модальное окно
-	d('modal_open').innerHTML = document.getElementById(el).innerHTML; // Вывести в модальном окне содержимое элемента "el"
+	d('modal').style.display = 'block'; 
+	d('modal_open').style.marginTop = '-'+h+'px'; 
+	d('modal_open').style.marginLeft = '-'+w+'px'; 
+	d('modal_open').style.display = 'block'; 
+	d('modal_open').innerHTML = document.getElementById(el).innerHTML;
 }
 
 function validate_form(form) {
